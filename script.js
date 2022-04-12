d3d248045f1c3b842fa55be93baaba8432b5d01e
piwiit <bouillaux@hotmail.fr>
2022-04-12 19:02:06 +0200

    add storage

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-const liste = document.getElementById('style');
 const input = document.querySelector('input[type=text]');
 const form = document.querySelector('form');
+
+const storageList = () => {
+  window.localStorage.todoList = ul.innerHTML;
+};
+
+const getStorageList = () => {
+  if (!window.localStorage.todoList === '') {
+    ul.innerHTML = window.localStorage.todoList;
+  } else {
+    console.log('test');
+    ul.innerHTML = ` <li>Cliquez sur un todo pour le supprimer</li>`;
+  }
+};
+
+window.addEventListener('load', getStorageList);
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const todo = document.createElement('li');
-  liste.appendChild(todo);
+  ul.appendChild(todo);
 
   todo.textContent = input.value;
   input.value = '';
+  storageList();
+});
+
+ul.addEventListener('click', (e) => {
+  if (e.target.classList.contains('linethrough')) {
+    e.target.remove();
+  } else {
+    e.target.classList.add('linethrough');
+  }
+  storageList();
 });
